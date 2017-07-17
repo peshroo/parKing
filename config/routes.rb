68124f5925ae80f57e619597252200e9c1ffbf09
Blueibis <jennie.li.2011@gmail.com>
2017-07-17 11:58:27 -0400

    create user edit partial, add js to render partial when link clicked, add delete user

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
+  get 'edit_user_form' => 'users#edit_user_form'
 
   resources :home
   resources :users
   resource :sessions, only: [:new, :create, :destroy]
-
 end
