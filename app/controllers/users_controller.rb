22d8cdcfddbd74ab4bd34a8dc6e7178dfc3b086d
jonathankim407 <jonathankim407@gmail.com>
2017-07-24 13:18:31 -0400

    add user avatar upload paperclip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -94,7 +94,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
   end
 
   def find_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724161409) do
+ActiveRecord::Schema.define(version: 20170724164558) do
 
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(version: 20170724161409) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
