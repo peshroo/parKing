ced82c3d3d13a654d8e1fb33db65dc603b5633f8
Blueibis <jennie.li.2011@gmail.com>
2017-07-25 17:29:32 -0400

    add and setup user profile listings

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,20 @@ Rails.application.routes.draw do
   get 'listing_search_form' => 'listings#listing_search_form'
   get 'listing_search' => 'listings#search'
   get 'show_user_bookings' => 'users#show_bookings'
+  get 'show_user_listings' => 'users#show_listings'
   get 'user_bookings' => 'users#user_bookings'
+  get 'user_listings' => 'users#user_listings'
   get 'review_form' => 'users#review_form'
   get 'edit_review_form' => 'users#edit_review_form'
   get 'listing_markers' => 'listings#markers'
+
   resource :reviews, only: [:new, :create, :update, :edit, :destroy]
   resources :home
   resources :users
   resource :sessions, only: [:new, :create, :destroy]
   resources :listings do
     resources :bookings
+    patch 'toggle'
   end
 
   get "/:page" => "pages#show"
