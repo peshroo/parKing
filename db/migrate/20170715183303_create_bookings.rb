efcfeb0a4c7023745f1a1c9912e696f39acb14a4
Blueibis <jennie.li.2011@gmail.com>
2017-07-27 13:57:01 -0400

    failed

--- db/migrate/20170715183303_create_bookings.rb
+++ db/migrate/20170715183303_create_bookings.rb
@@ -1,6 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[5.1]
   def change
     create_table :bookings do |t|
+      t.integer :user_id
+      t.integer :listing_id
+      t.integer :start_time
+      t.integer :end_time
+      t.date :date
 
       t.timestamps
     end
--- db/migrate/20170717163522_add_start_end_columns_to_listings_table.rb
+++ db/migrate/20170717163522_add_start_end_columns_to_listings_table.rb
@@ -1,6 +1,5 @@
 class AddStartEndColumnsToListingsTable < ActiveRecord::Migration[5.1]
   def change
-    add_column :listings, :start, :time
-    add_column :listings, :end, :time
+
   end
 end
