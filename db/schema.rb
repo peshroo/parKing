0d6ff50db7ffe977978ac34f08b5229057b3bc24
Blueibis <jennie.li.2011@gmail.com>
2017-07-28 16:30:21 -0400

    migrations,images from merging

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170724232633) do
+ActiveRecord::Schema.define(version: 20170728185532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id"
@@ -43,29 +41,26 @@ ActiveRecord::Schema.define(version: 20170724232633) do
     t.integer "price"
     t.integer "start"
     t.integer "end"
-    t.string "image"
     t.boolean "status", default: true
     t.bigint "user_id"
-    t.index ["user_id"], name: "index_listings_on_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "image_file_name"
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
-    t.integer "conversation_id"
-    t.integer "user_id"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
     t.boolean "read", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
-
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -76,7 +71,6 @@ ActiveRecord::Schema.define(version: 20170724232633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
