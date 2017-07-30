5abc5c9bdca265b32d99ce28afe07262e91f19bc
Blueibis <jennie.li.2011@gmail.com>
2017-07-30 19:55:04 -0400

    so much styling

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -85,6 +85,57 @@ document.addEventListener("DOMContentLoaded", function(){
         // })
         })
       })
+    } else if (document.getElementById('new_listing_map')) {
+      var options = {
+        zoom: 13,
+        center: {lat: 43.6532, lng: -79.3832}
+      }
+      var map = new google.maps.Map(document.getElementById('new_listing_map'), options);
+      var listingAddress = document.getElementById('listing_address')
+      var input = document.getElementById('pac-input');
+      var card = document.getElementById('pac-card');
+      map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+      var infowindow = new google.maps.InfoWindow();
+
+      var infowindowContent = document.getElementById('infowindow-content');
+
+      var autocomplete = new google.maps.places.Autocomplete(input);
+      autocomplete.bindTo('bounds', map);
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+        autocomplete.addListener('place_changed', function() {
+            infowindow.close();
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("No details available for input: '" + place.name + "'");
+          }
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+          listingAddress.value = address;
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindowContent.style.display = 'block'
+          infowindow.setContent(infowindowContent);
+          infowindow.open(map, marker);
+        });
     }
     }
 
